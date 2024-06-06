0b8d3a3681c128c8b0f6a45e1f3c1c281801b390
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-06-06 18:37:52 +0300

    Add detail urls to the forum app

--- game_forum/forum/urls.py
+++ game_forum/forum/urls.py
@@ -6,5 +6,18 @@ from .import views
 app_name = 'forum'
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',
+         views.index,
+         name='index'),
+    path('categories/<slug:category_slug>/',
+         views.category_detail,
+         name='category_detail'),
+    path('forums/<slug:forum_slug>/',
+         views.forum_detail,
+         name='forum_detail'),
+    path(
+        'topics/<slug:topic_slug>/',
+        views.topic_detail,
+        name='topic_detail'
+    )
 ]
