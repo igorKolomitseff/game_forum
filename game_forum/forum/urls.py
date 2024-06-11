4c4ec6445fde3f2b22827b2348836742502280bb
Igor Kolomytsev <igor.kolomitseff@yandex.ru>
2024-06-11 14:17:28 +0300

    Add urls for processing forms to the forum app

--- game_forum/forum/urls.py
+++ game_forum/forum/urls.py
@@ -15,9 +15,22 @@ urlpatterns = [
     path('forums/<slug:forum_slug>/',
          views.forum_detail,
          name='forum_detail'),
-    path(
-        'topics/<slug:topic_slug>/',
-        views.topic_detail,
-        name='topic_detail'
-    )
+    path('forums/<slug:forum_slug>/create_topic/',
+         views.topic_create,
+         name='create_topic'),
+    path('forums/<slug:forum_slug>/edit_topic/<slug:topic_slug>/',
+         views.topic_edit,
+         name='edit_topic'),
+    path('forums/<slug:forum_slug>/delete_topic/<slug:topic_slug>/',
+         views.topic_delete,
+         name='delete_topic'),
+    path('topics/<slug:topic_slug>/',
+         views.topic_detail,
+         name='topic_detail'),
+    path('topics/<slug:topic_slug>/edit_post/<int:post_id>/',
+         views.topic_detail,
+         name='edit_post'),
+    path('topics/<slug:topic_slug>/delete_post/<int:post_id>/',
+         views.topic_detail,
+         name='delete_post'),
 ]
